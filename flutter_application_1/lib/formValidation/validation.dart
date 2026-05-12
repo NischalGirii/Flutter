9e70992670cf500ddd46e0c7092402df7728398a
NischalGirii <NischalGirii@users.noreply.github.com>
2026-05-12 14:49:27 +0545

    validator for email, password, phoneNumber

--- flutter_application_1/lib/formValidation/validation.dart
+++ flutter_application_1/lib/formValidation/validation.dart
@@ -10,7 +10,7 @@ class Validation extends StatefulWidget {
 class _ValidationState extends State<Validation> {
   @override
     final _formKey = GlobalKey<FormState>();
-    TextEditingController usernameController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     TextEditingController phoneController = TextEditingController();
 
@@ -25,31 +25,70 @@ class _ValidationState extends State<Validation> {
         child: Column(
           children: [
             SizedBox(height: 40,),
-            TextField(
-              controller: usernameController,
+            TextFormField(
+              controller: emailController,
               decoration: InputDecoration(
-                labelText: "Username"
+                labelText: "Email"
               ),
-        
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return "please enter email";
+                }
+                if(!value.contains("@")){
+                  return "Enter valid Email";
+                }
+                return null;
+              } ,
               
             ),
-            TextField(
+            TextFormField(
               controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 
                 labelText: "Password"
               ),
+
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return "please enter password";
+                }
+                if(value.length <= 8) {
+                  return "Password must be atleast eight characters";
+                }
+                return null;
+              },
         
               
             ),
-            TextField(
+            TextFormField(
               controller: phoneController,
               decoration: InputDecoration(
                 labelText: "Phone Number"
               ),
+
+              validator: (value) {
+                if(value == null || value.isEmpty) {
+                  return "please enter phone number";
+                }
+                if(value.length < 10) {
+                  return "Phone Number must be atleast ten characters";
+                }
+                return null;
+              },
+
         
               
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(onPressed: () {},
+               child: Text("Submit"),
+               style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blueAccent
+               ),),
             )
           ],
         ),
